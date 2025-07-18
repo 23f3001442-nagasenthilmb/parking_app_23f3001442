1afe1fe71dea1fe0836a2f14579589a6175843cc
23f3001442-nagasenthilmb <23f3001442@ds.study.iitm.ac.in>
2025-07-18 06:39:00 +0530

    Modified config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 from app import app, db
-app.config('SQLACHEMY_DATABASE_URI') = os.getenv('SQLACHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 
-app.config('SQLALCHEMY_TRACK_MODIFICATION') = os.getenv('SQLACHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
